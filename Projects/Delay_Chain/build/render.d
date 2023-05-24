752cf50257851cc41c395d00cf480fe7f5284641
Lehel Török <lehel.toeroek@student.kug.ac.at>
2023-05-24 08:02:16 +0000

    Upload New File

---
+++ Projects/Delay_Chain/build/render.d
@@ -0,0 +1,52 @@
+/root/Bela/projects/effect_cape_git/build/render.o: \
+  /root/Bela/projects/effect_cape_git/render.cpp include/Bela.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
+  /usr/xenomai/include/cobalt/time.h include/Utilities.h \
+  libraries/libpd/libpd.h include/DigitalChannelManager.h \
+  libraries/Midi/Midi.h libraries/Scope/Scope.h \
+  libraries/Encoder/Encoder.h libraries/Pipe/Pipe.h \
+  libraries/Trill/Trill.h include/I2c.h \
+  /usr/xenomai/include/cobalt/fcntl.h \
+  /usr/xenomai/include/cobalt/sys/ioctl.h libraries/Gui/Gui.h \
+  include/JSON.h include/JSONValue.h include/DataBuffer.h
+
+include/Bela.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+include/GPIOcontrol.h:
+
+/usr/xenomai/include/cobalt/time.h:
+
+include/Utilities.h:
+
+libraries/libpd/libpd.h:
+
+include/DigitalChannelManager.h:
+
+libraries/Midi/Midi.h:
+
+libraries/Scope/Scope.h:
+
+libraries/Encoder/Encoder.h:
+
+libraries/Pipe/Pipe.h:
+
+libraries/Trill/Trill.h:
+
+include/I2c.h:
+
+/usr/xenomai/include/cobalt/fcntl.h:
+
+/usr/xenomai/include/cobalt/sys/ioctl.h:
+
+libraries/Gui/Gui.h:
+
+include/JSON.h:
+
+include/JSONValue.h:
+
+include/DataBuffer.h:
